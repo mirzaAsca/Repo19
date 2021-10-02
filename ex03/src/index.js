1ca4e7f7b556d7ff5f5305e099c5348d9bc1287a
mirzaAsca <asca_mirza7@hotmail.com>
2021-10-02 10:15:13 +0200

    ex04, ex03

--- ex03/src/index.js
+++ ex03/src/index.js
@@ -1,3 +1,5 @@
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
@@ -6,7 +8,7 @@ class App extends React.Component {
   render() {
   return (
     <div className="App">
-    <h1>Hello, React!</h1>
+    <h1>Hello, Padawans!</h1>
     </div>
   )
   }
